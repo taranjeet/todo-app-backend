2b870e1bd59b9e1616f8fc79f895a930db10303e
Taranjeet Singh <reachtotj@gmail.com>
2018-07-03 21:11:47 +0530

    simple app: explicitly set permission and authentication to None

--- simple/views.py
+++ simple/views.py
@@ -7,8 +7,12 @@ from .serializers import TodoSerializer
 class TodoList(generics.ListCreateAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+    authentication_classes = ()
+    permission_classes = ()
 
 
 class TodoDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = TodoSerializer
     queryset = Todo.objects.all()
+    authentication_classes = ()
+    permission_classes = ()
